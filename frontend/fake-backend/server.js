adf4f37dff470c4cb559bb82f6db41bce8886d5c
Ian Kusner <iankush1317@gmail.com>
2021-04-01 12:53:01 -0400

    cleaned up server code

--- frontend/fake-backend/server.js
+++ frontend/fake-backend/server.js
@@ -11,33 +11,25 @@ server.use(middlewares)
 
 server.post("/api/login", (req, res) => {
   const body = req.body
-  console.log("body", body)
 
   const result = db.users.find((user) => {
-    console.log(user)
-    console.log(body.email, body.password)
     return user.email === body.email && user.password === body.password
   })
-  console.log("result", result)
   if (result) {
     let { id, password, ...user } = result
     return res.status(200).jsonp(user)
-  } else {
-    return res.status(401).jsonp({
-      error: "invalid email or password",
-    })
   }
+  return res.status(401).jsonp({
+    error: "invalid email or password",
+  })
 })
 
 server.post("/api/campaign", (req, res) => {
   const body = req.body
-  console.log(body)
 
-  const db = router.db
+  const router_db = router.db
 
-  const table = db.get("campaigns")
-
-  tables.push(body).write()
+  insert(router_db, "campaings", body)
 
   return res.status(200)
 })
@@ -45,41 +37,14 @@ server.post("/api/campaign", (req, res) => {
 function insert(db, collection, data) {
   const table = db.get(collection)
 
-  /*const user = table.find((user) => {
-    console.log(user)
-    return user.email == data.email
-  })
-
-  console.log(user)
-
-  if (user) {
-    return false
-  }*/
-  let userType = "donor"
-  const kent = data.email.indexOf("@kent.edu")
-  if (kent > -1) userType = "student"
-  table
-    .push({
-      id: table._length,
-      first_name: data.firstname,
-      last_name: data.lastname,
-      email: data.email,
-      password: data.password,
-      user_id: faker.datatype.uuid(),
-      user_type: userType,
-    })
-    .write()
-  //return true
+  table.push(data).write()
 }
 
 server.post("/api/user", (req, res) => {
   const body = req.body
-  const head = req.header
 
-  console.log("body", body)
-  console.log("head", head)
-
-  //const db = router.db
+  //no sure how to push using 'db' instance created globally
+  const router_db = router.db
 
   const user = db.users.find((user) => {
     return user.email == body.email
@@ -87,18 +52,22 @@ server.post("/api/user", (req, res) => {
 
   if (user) {
     return res.status(401).jsonp({ message: "already exists" })
-  } else {
-    insert(router.db, "users", body)
-    return res.status(200).jsonp({ message: "message" })
   }
 
-  /*const inserted = insert(db, "users", body)
-
-  if (inserted) {
-    return res.status(200).jsonp({ message: "user create" })
-  } else {
-    return res.status(402).jsonp({ message: "user already exists" })
-  }*/
+  let userType = "donor"
+  const kent = body.email.indexOf("@kent.edu")
+  if (kent > -1) userType = "student"
+  const data = {
+    id: db.users.length,
+    first_name: body.firstname,
+    last_name: body.lastname,
+    email: body.email,
+    password: body.password,
+    user_id: faker.datatype.uuid(),
+    user_type: userType,
+  }
+  insert(router_db, "users", data)
+  return res.status(200).jsonp({ message: "message" })
 })
 
 server.use(router)
