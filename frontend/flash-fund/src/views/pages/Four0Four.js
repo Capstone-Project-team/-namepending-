17165767692a0b6412f5f28964d0bc4cbf840394
Ahmed Alsaleh <71618365+aalsale3@users.noreply.github.com>
2021-04-24 15:39:42 -0400

    updated Four0Four.js
    added an error image and link to go back to the home page

--- frontend/flash-fund/src/views/pages/Four0Four.js
+++ frontend/flash-fund/src/views/pages/Four0Four.js
@@ -1,8 +1,17 @@
-import React from "react"
+import React from "react";
+import { Link } from 'react-router-dom';
+import PageNotFound from './pagenotfound.png';
 
 //page for invalid route
 const Four0Four = (...props) => {
-  return <div>404</div>
+  return <div>
+    <img src={PageNotFound} alt="image not found" style={{ width: "100%", height: "100%", padding: "100px" }}></img>
+
+    <p style={{ textAlign: "center" }}>
+      <Link to="/" style={{ fontSize: 40 }}>Go to Home </Link>
+    </p>
+
+  </div >
 }
 
 export default Four0Four
