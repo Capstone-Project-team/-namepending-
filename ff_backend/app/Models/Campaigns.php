e82fb0dd040cf83ede45304a8217fad1cf6ff12f
Ian Kusner <iankush1317@gmail.com>
2021-04-28 02:06:04 -0400

    more changes

--- ff_backend/app/Models/Campaigns.php
+++ ff_backend/app/Models/Campaigns.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Campaigns extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'Description',
+        'Title',
+        'Donation Requested',
+        'Donation Collected'
+    ];
 }
