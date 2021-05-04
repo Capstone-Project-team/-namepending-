af547426b8a5b00cd6c036c7744c3407bbea3c6e
Ian Kusner <iankush1317@gmail.com>
2021-05-03 21:57:00 -0400

    main

--- frontend/flash-fund/src/views/pages/Four0Four.js
+++ frontend/flash-fund/src/views/pages/Four0Four.js
@@ -1,17 +1,24 @@
-import React from "react";
-import { Link } from 'react-router-dom';
-import PageNotFound from './pagenotfound.png';
+import React from "react"
+import { Link } from "react-router-dom"
+import PageNotFound from "./pagenotfound.png"
 
 //page for invalid route
 const Four0Four = (...props) => {
-  return <div>
-    <img src={PageNotFound} alt="image not found" style={{ width: "100%", height: "100%", padding: "100px" }}></img>
+  return (
+    <div>
+      <img
+        src={PageNotFound}
+        alt="image not found"
+        style={{ width: "100%", height: "100%", padding: "100px" }}
+      ></img>
 
-    <p style={{ textAlign: "center" }}>
-      <Link to="/" style={{ fontSize: 40 }}>Go to Home </Link>
-    </p>
-
-  </div >
+      <p style={{ textAlign: "center" }}>
+        <Link to="/" style={{ fontSize: 40 }}>
+          Go to Home{" "}
+        </Link>
+      </p>
+    </div>
+  )
 }
 
 export default Four0Four
